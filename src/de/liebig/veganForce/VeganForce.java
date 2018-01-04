c7d0ef21e82b22ba1444e1a145daaebef0a105c3
coder <coder@DESKTOP-T9EJ1BI.fritz.box>
2018-01-04 15:05:46 +0100

    Now the Vegan Force flies on the command of wasd in Space together with piggie (well but piggie on the other hand flies without controll through space,do)

--- src/de/liebig/veganForce/VeganForce.java
+++ src/de/liebig/veganForce/VeganForce.java
@@ -8,6 +8,8 @@ import javax.swing.ImageIcon;
  */
 public class VeganForce extends Actor {
 
+	private static final int STEP = 20;
+
 	/*
 	 * (non-Javadoc)
 	 * 
@@ -15,8 +17,23 @@ public class VeganForce extends Actor {
 	 */
 	@Override
 	public boolean keyPressed(char p_keyChar) {
-		// TODO Auto-generated method stub
 		System.out.println("Key code:\""+p_keyChar+"\"");
+		if (p_keyChar=='d') {
+			int mynewX=getX()+STEP;
+			setX(mynewX);
+		}
+		if (p_keyChar=='a') {
+			int mynewX=getX()-STEP;
+			setX(mynewX);
+		}
+		if (p_keyChar=='w') {
+			int mynewY=getY()-STEP;
+			setY(mynewY);
+		}
+		if (p_keyChar=='s') {
+			int mynewY=getY()+STEP;
+			setY(mynewY);
+		}
 		return super.keyPressed(p_keyChar);
 	}
 
