54133a5ca4e6556064d9c2cf0ddc899989f8e049
coder <coder@DESKTOP-T9EJ1BI.fritz.box>
2018-01-08 21:38:14 +0100

    Last bugs in planet and level behoben

--- src/de/liebig/veganForce/Planet.java
+++ src/de/liebig/veganForce/Planet.java
@@ -5,10 +5,11 @@ import javax.swing.ImageIcon;
 public class Planet extends Actor {
 
 	private static final int RAND_STEP = 300;
+
 	public Planet(World w) {
 		super(w, new ImageIcon(Planet.class.getResource("/bilder/planet101.png")));
-		setX((int)(((w.getWidth()-2*RAND_STEP) * Math.random())+RAND_STEP));
-		setY((int)(((w.getHeight()-2*RAND_STEP)* Math.random())+RAND_STEP));
+		setX((int) (((w.getWidth() - 2 * RAND_STEP) * Math.random()) + RAND_STEP));
+		setY((int) (((w.getHeight() - 2 * RAND_STEP) * Math.random()) + RAND_STEP));
 	}
 
 }
